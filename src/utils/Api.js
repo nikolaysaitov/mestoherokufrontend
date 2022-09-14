bc2e7f67fa33bf37c9898e722490eaef5b3e9d25
Nikolay Saitov <saitovkms@gmail.com>
2022-09-14 13:49:21 +0300

    1

---
+++ src/utils/Api.js
@@ -0,0 +1,117 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._headers = headers;
+    this._baseUrl = baseUrl; // тело конструктора
+  }
+
+  _getResponseData(res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json();
+  }
+
+  getProfile(token) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(this._getResponseData);
+  }
+
+  getInitialCards(token) {
+    return fetch(`${this._baseUrl}/cards`, {
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(this._getResponseData);
+  }
+
+  editProfile(name, about, token) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      credentials: 'include',
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then(this._getResponseData);
+  }
+  addCard(name, link, token) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    }).then(this._getResponseData);
+  }
+  deleteCard(id, token) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: "DELETE",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(this._getResponseData);
+  }
+
+  deleteLike(id, token) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: "DELETE",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(this._getResponseData);
+  }
+
+  addLike(id, token) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: "PUT",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(this._getResponseData);
+  }
+
+  updateAvatar(avatar, token) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        avatar,
+      }),
+    }).then(this._getResponseData);
+  }
+
+  // другие методы работы с API
+}
+
+export const api = new Api({
+  baseUrl: "https://mestoherokubackend.herokuapp.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
---
+++ src/utils/auth.js
@@ -0,0 +1,53 @@
+export let BASE_URL = "";
+const { NODE_ENV } = process.env;
+if (NODE_ENV === "production") {
+  BASE_URL = "https://mestoherokubackend.herokuapp.com";
+} else {
+  BASE_URL = "http://localhost:3000";
+}
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(res.status);
+}
+
+export const register = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  }).then(checkResponse);
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  }).then(checkResponse);
+};
+
+export const checkToken = () => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  }).then(checkResponse);
+};
